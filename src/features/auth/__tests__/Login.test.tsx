c1f22ea261839d055c12fc93e54b1f970ee15a43
trong nghia <trongnghialedinh@gmail.com>
2022-08-03 13:08:18 +0700

    setup jest

---
+++ src/features/auth/__tests__/Login.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Login from "../pages/Login";
+
+describe("Login component test", () => {
+  it("should return a text with content 'Login'", () => {
+    render(<Login />);
+    const textElement = screen.getByText("Login");
+    expect(textElement).toBeInTheDocument();
+  });
+});
--- src/features/auth/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "../pages/Login";
-
-test("Login component test", () => {
-  render(<Login />);
-  const textElement = screen.getByText("Login");
-  expect(textElement).toBeInTheDocument();
-});
