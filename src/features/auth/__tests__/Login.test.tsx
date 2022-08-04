5c8b8190b8068530791e587bd968dfc4f0ca4306
n3w-us3r <trongnghialedinh@gmail.com>
2022-08-04 17:25:14 +0700

    test if sonarcloud can spot the unit test reports

--- src/features/auth/__tests__/Login.test.tsx
+++ src/features/auth/__tests__/Login.test.tsx
@@ -4,7 +4,7 @@ import Login from "../pages/Login";
 describe("Login component test", () => {
   it("should return a text with content 'Login'", () => {
     render(<Login />);
-    const textElement = screen.getByText("Login");
+    const textElement = screen.getByText("abc");
     expect(textElement).toBeInTheDocument();
   });
 });
