8f3f2a5006c4e44c394807da28b4db883a0f3ac4
n3w-us3r <trongnghialedinh@gmail.com>
2022-08-08 08:59:05 +0700

    setup logging strategy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,15 @@
-export const API_BASE_URL = process.env.API_BASE_URL as string;
+import { LogLevel } from "@/logger/ConsoleLogger";
+
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL as string;
+
+// Logging configurations
+export type Environment = "development" | "production" | "staging";
+
+export const APP_ENV: Environment =
+  process.env.REACT_APP_APP_ENV === "production"
+    ? "production"
+    : process.env.REACT_APP_APP_ENV === "development"
+    ? "development"
+    : "staging";
+
+export const LOG_LEVEL: LogLevel = APP_ENV === "production" ? "warn" : "log";
