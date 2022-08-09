1a8dd488722bacc77f23b26cfafbb47cf66c5a5f
trong nghia <trongnghialedinh@gmail.com>
2022-08-09 10:18:45 +0700

    implement message box

---
+++ src/presentation/shared/components/MessageBox.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+type messageType = "error" | "warning" | "info" | "success";
+
+interface Props {
+  message: string;
+  open?: boolean;
+  type?: messageType;
+}
+
+export default function MessageBox({
+  open = true,
+  type = "info",
+  message,
+}: Props) {
+  const [isOpen, setIsOpen] = React.useState(open);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setIsOpen(false);
+  };
+
+  return (
+    <Snackbar
+      open={isOpen}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+    >
+      <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
