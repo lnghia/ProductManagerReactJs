ccecad2c097041a285cb0a352dcbe19329e73f40
n3w-us3r <trongnghialedinh@gmail.com>
2022-08-11 11:52:19 +0700

    set log level based on node_env

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,5 @@ import { LogLevel } from "../application/logger/ConsoleLogger";
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL as string;
 
-// Logging configurations
-export type Environment = "development" | "production" | "staging";
-
-export const APP_ENV: Environment =
-  process.env.REACT_APP_APP_ENV === "production"
-    ? "production"
-    : process.env.REACT_APP_APP_ENV === "development"
-    ? "development"
-    : "staging";
-
-export const LOG_LEVEL: LogLevel = APP_ENV === "production" ? "warn" : "log";
+export const LOG_LEVEL: LogLevel =
+  process.env.REACT_APP_NODE_ENV === "production" ? "warn" : "log";
