ca1f3cb95010d34dddb4541b64ad219c430298e6
Dinh Thai <dinhtvv.pnv@gmail.com>
2022-08-20 17:01:36 +0700

    feature/apply-popup

--- src/presentation/product/pages/CreateProduct.tsx
+++ src/presentation/product/pages/CreateProduct.tsx
@@ -1,145 +1,131 @@
-import { Button, Container, TextField, Typography, MenuItem } from '@mui/material';
-import React, { Component, useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-import { createNewProduct } from '../../../infrastructure/api/product-api';
-import "../../shared/components/css/Table.css";
-import { CategoryDTO } from "../../../infrastructure/api/dtos/CategoryDTO";
 import { fetchCategories } from "../../../infrastructure/api/category-api";
-import "../../shared/components/css/Table.css";
+import { createNewProduct } from '../../../infrastructure/api/product-api';
+import { CategoryDTO } from "@/infrastructure/api/dtos/CategoryDTO";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import ImportDialogProps from "@/presentation/shared/ImportDialogProps";
 
-interface Role {
-  name: string;
-  id: number;
-}
+const CreateProduct = (props: ImportDialogProps) => {
+    const { isOpen } = props;
 
-const roles: Role[] = [
-  {
-    name: "admin",
-    id: 1,
-  },
-  {
-    name: "user",
-    id: 2,
-  },
-];
+    const handleClose = () => {
+        props.closeDialog(null);
+    };
 
-const initialValueForCategories: CategoryDTO[] = [];
-
-export default function CreateProduct() {
-
+    const initialValueForCategories: CategoryDTO[] = [];
     const [categories, setCategories] = useState(initialValueForCategories);
-    const [selectedCategory, setSelectedCategory] = useState(0); 
+    const [selectedCategory, setSelectedCategory] = useState(0);
     const [name, setName] = useState('')
     const [description, setDescrition] = useState('')
     const [nameError, setNameError] = useState(false)
     const [price, setPrice] = useState(0)
 
-    const navigate = useNavigate();
-
     useEffect(() => {
-      const getCategories = async () => {
-        const result = await fetchCategories();
-        setCategories(result);
-      };
-  
-      getCategories();
+        const getCategories = async () => {
+            const result = await fetchCategories();
+            setCategories(result);
+        };
+
+        getCategories();
     }, []);
 
 
 
-    const handleSubmit = (e: React.FormEvent<HTMLInputElement| HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLInputElement | HTMLFormElement>) => {
         e.preventDefault()
 
         setNameError(false)
         if (name == '') {
             setNameError(true)
         }
-      
         if (name && description) {
-            let product = {name: name, price: price, description: description, categoryId: selectedCategory};
-            // console.log(product)
-            let result = createNewProduct(product);
-            console.log(result);
+            let product = { name: name, price: price, description: description, categoryId: selectedCategory };
+            createNewProduct(product);
         }
+        props.closeDialog(null);
     }
 
     const handleChangeCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSelectedCategory(parseInt(event.target.value));
+        setSelectedCategory(parseInt(event.target.value));
     };
 
-    const handleCancel = () => {
-        navigate('/products');
-    }
-
     return (
-        <Container className="container">
-            <Typography align="center" variant="h3">
+        <Dialog open={isOpen} onClose={handleClose}>
+            <DialogTitle>
                 Create New Product
-            </Typography>
-            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
-                <TextField
-                    onChange={(e) => setName(e.target.value)}
-                    label="Product Name"
-                    variant='outlined'
-                    color='secondary'
-                    required
-                    error={nameError}
-                    style={{width: 400}}
-                />
-                <br/>
-                <TextField
-                    onChange={(e) => setDescrition(e.target.value)}
-                    label="Description"
-                    variant='outlined'
-                    color='secondary'
-                    fullWidth
-                    multiline
-                    rows={4}
-                />
-                <br/>
-                <div>
-                  <TextField
-                      type="number"
-                        name="price"
-                        label="Price $"
-                        variant="filled"
-                        value={price}
-                        style={{width: 200}}
-                        onChange={e => setPrice(parseInt(e.target.value))}
-                    />
-                </div>
-                <br/>
-                <TextField
-                  id="outlined-select-currency"
-                  select
-                  label="Select category"
-                  value={selectedCategory}
-                  onChange={handleChangeCategory}
-                  helperText="Please select category"
-                  className="inputs-inside"
-                  style={{width: 200}}
-                >
-                  {categories.map((option) => (
-                    <MenuItem key={option.id} value={option.id}>
-                      {option.name}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <br/>
-                <div>
-                  <Button
-                      type="submit"
-                      variant='contained'
-                  >Save</Button>
-                  <Button
-                      onClick={handleCancel}
-                      variant='outlined'
-                  >Cancel</Button>
-                </div>
-                
-            </form>
-            
+            </DialogTitle>
 
-        </Container>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                <DialogContent>
+                    <TextField
+                        onChange={(e) => setName(e.target.value)}
+                        label="Product Name"
+                        variant='outlined'
+                        color='secondary'
+                        required
+                        error={nameError}
+                        style={{ width: 400 }}
+                    />
+                    <br />
+                    <TextField
+                        onChange={(e) => setDescrition(e.target.value)}
+                        label="Description"
+                        variant='outlined'
+                        color='secondary'
+                        fullWidth
+                        multiline
+                        rows={4}
+                    />
+                    <br />
+                    <div>
+                        <TextField
+                            type="number"
+                            name="price"
+                            label="Price $"
+                            variant="filled"
+                            value={price}
+                            style={{ width: 200 }}
+                            onChange={e => setPrice(parseInt(e.target.value))}
+                        />
+                    </div>
+                    <br />
+                    <TextField
+                        id="outlined-select-currency"
+                        select
+                        label="Select category"
+                        value={selectedCategory}
+                        onChange={handleChangeCategory}
+                        helperText="Please select category"
+                        className="inputs-inside"
+                        style={{ width: 200 }}
+                    >
+                        {categories.map((option) => (
+                            <MenuItem key={option.id} value={option.id}>
+                                {option.name}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <br />
+
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        type="submit"
+                        variant='contained'
+                    >Save</Button>
+                    <Button
+                        onClick={handleClose}
+                        variant='outlined'
+                    >Cancel</Button>
+                </DialogActions>
+            </form>
+        </Dialog>
     );
+}
+
+
+export default CreateProduct;
+
+function result(result: any) {
+    throw new Error("Function not implemented.");
 }
---
+++ src/presentation/shared/ImportDialogProps.tsx
@@ -0,0 +1,6 @@
+interface ImportDialogProps {
+    isOpen: boolean,
+    closeDialog: (result: any) => any
+}
+
+export default ImportDialogProps;
